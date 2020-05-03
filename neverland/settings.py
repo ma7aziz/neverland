e5082197ca3a857b825b26af0749749cf553098a
mahmoud <ma7.aelaziz@gmail.com>
2020-05-03 23:56:38 +0200

    debug false

--- neverland/settings.py
+++ neverland/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'c_^)z07=0p1ne(&jh0yvg99i2^2@*g8hkw+^$@dco_f068(=)c'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['161.35.97.185']
+DEBUG = False
+ALLOWED_HOSTS = ['161.35.97.185', '*']
 
 
 # Application definition
