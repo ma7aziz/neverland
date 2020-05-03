abc50a3621a8ed4f4b9c4a22ea427302d1212a19
mahmoud <ma7.aelaziz@gmail.com>
2020-05-03 23:00:35 +0200

    changed header

--- neverland/settings.py
+++ neverland/settings.py
@@ -100,6 +100,12 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
