e8c74c5902a30cf9c1923d3a6ccf9d489dfbbeeb
mahmoud <ma7.aelaziz@gmail.com>
2020-05-06 18:10:47 +0200

    domain

--- neverland/settings.py
+++ neverland/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'c_^)z07=0p1ne(&jh0yvg99i2^2@*g8hkw+^$@dco_f068(=)c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['neverlandcostume.com']
+ALLOWED_HOSTS = ['neverlandcostume.com' , '161.35.97.185']
 
 
 # Application definition
