122e5a4951582fca79ee31c3661049095489ae67
mahmoud <ma7.aelaziz@gmail.com>
2020-05-15 01:02:26 +0200

    fix settings.py

--- neverland/settings.py
+++ neverland/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'neverland.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'neverland',
-#         'USER': 'neverland',
-#         'PASSWORD': 'never@brain123',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'neverland',
+        'USER': 'neverland',
+        'PASSWORD': 'never@brain123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
